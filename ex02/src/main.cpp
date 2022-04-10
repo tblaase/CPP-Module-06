8c9039923e63c7448f5b988c3c1193fdc9521e74
tblaase <tblaase@student.42.fr>
2022-04-10 17:47:28 +0200

    ex02 done

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 16:46:48 by tblaase           #+#    #+#             */
-/*   Updated: 2022/04/10 17:00:39 by tblaase          ###   ########.fr       */
+/*   Updated: 2022/04/10 17:44:34 by tblaase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@
 
 #include <cstdlib>
 #include <cstdio>
+#include <unistd.h>
 
 static Base *generate(void)
 {
-	srand(time(NULL));
 	switch (rand() % 3)
 	{
 	case 0:
@@ -33,14 +33,70 @@ static Base *generate(void)
 		return (new C());
 		break;
 	default:
-		perror("Something went wrog with the radom generator");
+		perror("Something went wrong with the random generator");
 		return (NULL);
+	}
+}
+
+static void identify(Base *Test)
+{
+	if (dynamic_cast<A *>(Test))
+		std::cout << "A is the identified type" << std::endl;
+	else if (dynamic_cast<B *>(Test))
+		std::cout << "B is the identified type" << std::endl;
+	else if (dynamic_cast<C *>(Test))
+		std::cout << "C is the identified type" << std::endl;
+	else
+		std::cout << "unknown type" << std::endl;
+}
+
+static int i = 0;
+static std::string classes[] = {"A", "B", "C"};
+
+static void identify(Base &Test)
+{
+	while (i < 3)
+	{
+		try
+		{
+			if (i == 0)
+				dynamic_cast<A &>(Test);
+			else if (i == 1)
+				dynamic_cast<B &>(Test);
+			else if (i == 2)
+				dynamic_cast<C &>(Test);
+			else
+				std::cout << "unknow type" << std::endl;
+		}
+		catch (std::exception &e)
+		{
+			// std::cout << e.what() << std::endl; //enable to see what exception was cought
+			i++;
+			identify(Test);
+			return;
+		}
+		std::cout << classes[i] << " is the identified type" << std::endl;
+		i = 0;
+		break;
 	}
 }
 
 int main()
 {
-	Base *Test = generate();
-	std::cout << Test << std::endl;
+	srand(time(NULL));
+	for (int j = 0; j < 5; j++)
+	{
+		Base *Test = generate();
+		if (Test == NULL)
+			return (1);
+		else
+		{
+			identify(Test);
+			identify(*Test);
+			delete (Test);
+
+			std::cout << std::endl;
+		}
+	}
 	return (0);
 }
